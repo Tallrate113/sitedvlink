f176371a4e10c0582f6e74cb4d398c2d52696233
Tallrate113 <nikolay.prohorov.77@gmail.com>
2023-05-18 18:43:14 +1000

    Connecting css, and adding code to html and css

--- sitedvlink/dvlink/views.py
+++ sitedvlink/dvlink/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+
+from .models import *
 def index(request):
-    return render(request, 'dvlink/index.html',{'title':'Главная страница'})
+    return render(request, 'dvlink/index.html', {'title': 'Главная страница'})
 
 def account(request):
-    return render(request, 'dvlink/account.html',{'title':'Аккаунт'})
+    return render(request, 'dvlink/account.html', {'title': 'Аккаунт'})
